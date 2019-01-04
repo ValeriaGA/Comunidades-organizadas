3122b6f81aeda9aaf590f7042f790e0d3f23cc28
Manrique <manriquedv@estudiantec.cr>
2019-01-04 10:15:21 -0600

    Arreglo de Detalles de Reporte

--- public/js/dropdownTableRows.js
+++ public/js/dropdownTableRows.js
@@ -6,4 +6,18 @@ $(document).ready(function() {
           modal.style.display = "none";
       }
   }
+  
+  
+    //Fixing jQuery Click Events for the iPad
+    var ua = navigator.userAgent,
+    event = (ua.match(/iPad/i)) ? "touchstart" : "click";
+    if ($('.table').length > 0) {
+        $('.table .header').on(event, function() {
+            $(this).toggleClass("active", "").nextUntil('.header').css('display', function(i, v) {
+            return this.style.display === 'table-row' ? 'none' : 'table-row';
+            });
+        });
+    }
+
+
   });
