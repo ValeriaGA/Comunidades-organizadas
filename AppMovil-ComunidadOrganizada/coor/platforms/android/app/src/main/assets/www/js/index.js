e88dd506b66c2366f8a82cf8e40d605acc71d56c
ValeriaGA <valeriaga1000@gmail.com>
2019-01-13 20:57:05 -0600

    App movil

--- AppMovil-ComunidadOrganizada/coor/platforms/android/app/src/main/assets/www/js/index.js
+++ AppMovil-ComunidadOrganizada/coor/platforms/android/app/src/main/assets/www/js/index.js
@@ -1,46 +1,94 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-var app = {
-    // Application Constructor
-    initialize: function() {
-        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
-    },
+$(document).ready(function(){
 
-    // deviceready Event Handler
-    //
-    // Bind any cordova events here. Common events are:
-    // 'pause', 'resume', etc.
-    onDeviceReady: function() {
-        this.receivedEvent('deviceready');
-    },
+var myDB;
+//Open Database Connection
+document.addEventListener("deviceready",onDeviceReady,false);
+function onDeviceReady(){
+myDB = window.sqlitePlugin.openDatabase({name: "mySQLite.db", location: 'default'});
+}
+//Create new table
+$("#createTable").click(function(){
+    myDB.transaction(function(transaction) {
+    transaction.executeSql('CREATE TABLE IF NOT EXISTS phonegap_pro (id integer primary key, title text, desc text)', [],
+        function(tx, result) {
+            alert("Table created successfully");
+        }, 
+        function(error) {
+              alert("Error occurred while creating the table.");
+        });
+    });
+});
 
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+//Insert New Data
+$("#insert").click(function(){
+  var title=$("#title").val();
+  var desc=$("#desc").val();
+  console.log(title +""+ desc);
+  myDB.transaction(function(transaction) {
+        var executeQuery = "INSERT INTO phonegap_pro (title, desc) VALUES (?,?)";             
+        transaction.executeSql(executeQuery, [title,desc]
+            , function(tx, result) {
+                 alert('Inserted');
+            },
+            function(error){
+                 alert('Error occurred'); 
+            });
+    });
+});
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+//Display Table Data
+$("#showTable").click(function(){
+  $("#TableData").html("");
+  myDB.transaction(function(transaction) {
+  transaction.executeSql('SELECT * FROM phonegap_pro', [], function (tx, results) {
+       var len = results.rows.length, i;
+       $("#rowCount").html(len);
+       for (i = 0; i < len; i++){
+          $("#TableData").append("<tr><td>"+results.rows.item(i).id+"</td><td>"+results.rows.item(i).title+"</td><td>"+results.rows.item(i).desc+"</td><td><a href='edit.html?id="+results.rows.item(i).id+"&title="+results.rows.item(i).title+"&desc="+results.rows.item(i).desc+"'>Edit</a> &nbsp;&nbsp; <a class='delete' href='#' id='"+results.rows.item(i).id+"'>Delete</a></td></tr>");
+       }
+    }, null);
+  });
+});
 
-        console.log('Received Event: ' + id);
-    }
-};
+//Delete Data from Database
+$(document.body).on('click', '.delete' ,function(){
+  var id=this.id;
+  myDB.transaction(function(transaction) {
+    var executeQuery = "DELETE FROM phonegap_pro where id=?";
+    transaction.executeSql(executeQuery, [id],
+      //On Success
+      function(tx, result) {alert('Delete successfully');},
+      //On Error
+      function(error){alert('Something went Wrong');});
+  });
+});
 
-app.initialize();
+
+//Delete Tables
+$("#update").click(function(){
+  var id=$("#id").text();
+  var title=$("#title").val();
+  var desc=$("#desc").val()
+  myDB.transaction(function(transaction) {
+    var executeQuery = "UPDATE phonegap_pro SET title=?, desc=? WHERE id=?";
+    transaction.executeSql(executeQuery, [title,desc,id],
+      //On Success
+      function(tx, result) {alert('Updated successfully');},
+      //On Error
+      function(error){alert('Something went Wrong');});
+  });
+});
+
+$("#DropTable").click(function(){
+    myDB.transaction(function(transaction) {
+        var executeQuery = "DROP TABLE  IF EXISTS phonegap_pro";
+        transaction.executeSql(executeQuery, [],
+            function(tx, result) {alert('Table deleted successfully.');},
+            function(error){alert('Error occurred while droping the table.');}
+        );
+    });
+});
+
+});
+
+
--- AppMovil-ComunidadOrganizada/coor/platforms/browser/www/cordova_plugins.js
+++ AppMovil-ComunidadOrganizada/coor/platforms/browser/www/cordova_plugins.js
@@ -1,9 +1,37 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = [];
+module.exports = [
+    {
+        "file": "plugins/cordova-sqlite-legacy-build-support/www/SQLitePlugin.js",
+        "id": "cordova-sqlite-legacy-build-support.SQLitePlugin",
+        "pluginId": "cordova-sqlite-legacy-build-support",
+        "clobbers": [
+            "SQLitePlugin"
+        ]
+    },
+    {
+        "file": "plugins/uk.co.workingedge.cordova.plugin.sqliteporter/www/sqlitePorter.js",
+        "id": "uk.co.workingedge.cordova.plugin.sqliteporter.sqlitePorter",
+        "pluginId": "uk.co.workingedge.cordova.plugin.sqliteporter",
+        "clobbers": [
+            "cordova.plugins.sqlitePorter"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-sqlite-2/dist/sqlite-plugin.js",
+        "id": "cordova-plugin-sqlite-2.sqlitePlugin",
+        "pluginId": "cordova-plugin-sqlite-2",
+        "clobbers": [
+            "sqlitePlugin"
+        ]
+    }
+];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "cordova-plugin-whitelist": "1.3.3"
+    "cordova-plugin-whitelist": "1.3.3",
+    "cordova-sqlite-legacy-build-support": "1.3.5",
+    "uk.co.workingedge.cordova.plugin.sqliteporter": "1.1.0",
+    "cordova-plugin-sqlite-2": "1.0.4"
 }
 // BOTTOM OF METADATA
 });
