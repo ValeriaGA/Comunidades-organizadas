2fa1b3f893386316804e84972b62ad8c388d6218
ValeriaGA <valeriaga1000@gmail.com>
2019-01-17 18:03:20 -0600

    Add news movil app

--- AppMovil-ComunidadOrganizada/coor/www/php/new_news.php
+++ AppMovil-ComunidadOrganizada/coor/www/php/new_news.php
@@ -3,45 +3,28 @@
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST, PATH, PUT, DELETE, OPTIONS');
 
-require 'Credentials.php';
+//require 'Credentials.php';
+    $host = "localhost";
+    $user = "root";
+    $passwd = "root";
+    $database = "comunidadesorganizadas";
 
     $con = mysqli_connect($host, $user, $passwd, $database) or die("connection error");
-    $email = $_POST['email'];
-    $password = $_POST['password'];
-    $new_username = $_POST['new_username'];
-    $first_lastname = $_POST['first_lastname'];
-    $second_lastname = $_POST['second_lastname'];
-    $official_id = $_POST['official_id'];
-    $gender = $_POST['gender'];
-    $foreigner = $_POST['foreigner'];
-    $photo = $_POST['photo'];
+    $title = $_POST['email'];
+    $description = $_POST['password'];
+    $community = $_POST['comunnity'];
 
-    if(isset($_POST['register']))
-    {   9
-        $query = mysqli_query($con, "SELECT * FROM `users` WHERE `email`='$email'");
-        $register = mysqli_num_rows($query);
-        if($register == 0)
-        {
-            $insertPeople = mysqli_query($con,"INSERT INTO `people` (`name`, `last_name`, `second_last_name`, `official_id`, `gender`,`foreigner`) VALUES ('$new_username', '$first_lastname','$second_lastname', '$official_id','$gender','$foreigner')");
-            
-            $person_id = mysqli_insert_id($con);
-            
-            $insertUser = mysqli_query($con,"INSERT INTO `users` (`role_id`, `person_id`, `email`, `email_verified_at`, `password`,`avatar_path`) VALUES ('1', '1','$email', date('Y-m-d H:i:s'),'$password','$photo')");
-            if($insertUser)
+    if(isset($_POST['news']))
+    {   
+        
+            $insertNews = mysqli_query($con,"`reports` (`title`, `description`, `community_group_id`, `sub_cat_report_id`, `user_id`, `state_id`) VALUES ( '$title', '$description', '$community', '1', '1', '1');");
+          
+            if($insertNews)
                 echo "success";
             else
                 echo "error";
         }
-        else if($registerUser != 0)
-            echo "exist";
-    }
-    else if(isset($_POST['login']))
-    {
-        $login = mysqli_num_rows(mysqli_query($con, "SELECT * FROM `users` WHERE `email`='$email' AND `password`='$password'"));
-        if($login != 0)
-            echo "success";
-        else
-            echo "error";
-    }
+    
+    
     mysqli_close($con);
 ?>
